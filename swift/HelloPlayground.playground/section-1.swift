5ff4fbf2d462a852f53d41bb08f3f801546812e2
Mrudul P <mrudul@metawatch.com>
2015-03-25 15:19:38 +0200

    Initial commit of swift playground project

---
+++ swift/HelloPlayground.playground/section-1.swift
@@ -0,0 +1,26 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+println("str");
+
+// Implicit Variable type
+var myVar = 42; // Variable
+let myConstant = 45; // Constant value
+
+// Explicit Variable type
+var myVar2 :Int = 43;
+let myConstant2 :Float = 48;
+
+// iBook EXPERIMENT Solution:
+let explicitFloatConst :Float = 4;
+
+//Conversion of Values
+let label = "The width is"
+let width = 100
+let widthlabel = label + String(width)
+
+let name = "John"
+let sumFloat = 5.0 + 10.0
+let convertedString = "Hello \(name), here is the car which is \(sumFloat) long"
